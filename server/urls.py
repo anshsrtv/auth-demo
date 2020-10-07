eaf469b358a92a7847d97061c0f353b8565eadde
anshsrtv <ansh_srtv@outlook.com>
2020-10-08 00:14:37 +0530

    feat: Added list view

--- server/urls.py
+++ server/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from quiz.views import list_quizzes
+from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('list/', list_quizzes),
 ]
