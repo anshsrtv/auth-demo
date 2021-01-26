d9a979771fc4b015572ed31b3aee13035c1c8439
Saloni-NITRR <ansh_srtv@outlook.com>
2021-01-26 13:40:03 +0530

    fix: Setup Auth

--- server/urls.py
+++ server/urls.py
@@ -1,20 +1,16 @@
 from django.conf.urls import url
 from django.contrib import admin
-from quiz.views import list_quizzes, show_quiz, check_answer, index
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from user.views import signup, log_in, user_logout
+from user.views import signup, log_in, user_logout, home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('', index),
-    path('list/', list_quizzes, name='list_quiz'),
-    path('quiz/<int:quiz_id>/', show_quiz, name='show_quiz'),
-    path('attempt/<int:question_id>/', check_answer, name='check_answer'),
+    path('', home, name='home'),
     path('signup/', signup, name='signup'),
     path('login/', log_in, name='login'),
     path('logout/', user_logout, name='logout'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 urlpatterns += staticfiles_urlpatterns()
--- user/views.py
+++ user/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from .forms import SignupForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+@login_required(login_url='/login/')
+def home(request):
+    username = request.user.username
+    return HttpResponse("Hi {}, Welcome Home!".format(username))
 
 def signup(request):
    if request.method == 'POST':
@@ -11,7 +18,7 @@ def signup(request):
        if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('list_quiz')
+            return redirect('home')
    else:
        form = SignupForm()
    return render(request,'signup.html',{'form':form})
@@ -20,10 +27,9 @@ def log_in(request):
    if request.method == 'POST':
        user=authenticate(request,username=request.POST['username'],
        password=request.POST['password'])
-       print(user)
        if user is not None:
            login(request,user)
-           return redirect('list_quiz')
+           return redirect('home')
        else:
            messages.error(request,'Invalid Credentials')
            return redirect('login')
