ad284a2721f80f5bf29ced4b0b9bd399c25e7825
Ansh Srivastava <ansh_srtv@outlook.com>
2021-01-26 13:40:58 +0530

    Delete quiz directory

--- quiz/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Quiz, Question, Answer, Attempt
-
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Attempt)
-admin.site.register(Answer)
